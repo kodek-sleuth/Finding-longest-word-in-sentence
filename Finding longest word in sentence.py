440aa449a0a7d4bc186269414ee070e69f237a87
kodek2000 <41841554+kodek2000@users.noreply.github.com>
2018-12-01 18:27:08 +0300

    Add files via upload

--- Finding longest word in sentence.py
+++ Finding longest word in sentence.py
@@ -1,15 +1,14 @@
-enter=input("Enter text in here: ")
-x= enter.split(' ')
-li=[]
+text=input("Enter your text here: ")
+splittext=text.split(" ")
 mydict=dict()
+empt=[]
+for i in splittext:
+    mydict[len(i)]=i
+    for n in mydict.keys():
+        empt.append(n)
+        maxi=max(empt)
+mykey = maxi
+description=mydict.get(maxi)
+print('Longest word in sentence is '+ description)
 
-for i in x:
-    k=len(i)
-    mydict[k]=i
-print(mydict)
 
-numbers=mydict
-
-mydictkey=int(input("Enter Highest Integer in Dictionary: "))
-descriptionValue=numbers.get(mydictkey)
-print('Longest Word is '+ descriptionValue)
